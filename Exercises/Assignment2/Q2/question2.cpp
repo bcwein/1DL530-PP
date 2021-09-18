275a6f465e5593088129334868f4e391343cc5dd
ThameezBo <thameez.bodhanya@allangray.co.za>
2021-09-18 17:42:07 +0200

    fix: cleanup

---
+++ Exercises/Assignment2/Q2/question2.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <pthread.h>
+#include <chrono>
+#include <math.h>
+
+using namespace std;
+
+int numThreads;
+int *results;
+long long maxNumber;
+double a = 0, b = 1, w, results;
+
+struct Config
+{
+  int startI;
+  int numTrapPerThread;
+} * cfg;
+
+pthread_mutex_t results_mutex;
+
+double function(double x)
+{
+  return 4 / (1 + (x * x));
+}
+
+void *calculateFactorial(void *conf)
+{
+  Config *cfg = (Config *)conf;
+  double localResults;
+
+  for (int i = 0; i < cfg->numTrapPerThread; i++)
+  {
+    int pos = cfg->startI * cfg->numTrapPerThread + i;
+    if (pos == 0 || pos == maxNumber)
+    {
+      continue;
+    }
+    localResults = localResults + 2 * function(a + ((pos)*w));
+  }
+
+  pthread_mutex_lock(&results_mutex);
+  results += localResults;
+  pthread_mutex_unlock(&results_mutex);
+
+  pthread_exit(0);
+}
+
+int getNumTrapPerThread(int i, int numThreads, int maxNumber)
+{
+  // Add surplus to the last thread
+  if (i == numThreads - 1)
+  {
+    return (maxNumber / numThreads) + (maxNumber % numThreads);
+  }
+  return maxNumber / numThreads;
+}
+
+void markNonPrimes(int startNumber, int maxNumber)
+{
+  for (; startNumber < maxNumber; startNumber++)
+  {
+    // TODO:add a read mutex here
+    if (results[startNumber] == 1)
+    {
+      continue;
+    }
+    int numberSqrd = startNumber * startNumber;
+    int multiple = 1;
+    while (multiple * numberSqrd < maxNumber)
+    {
+      // TODO:add a write mutex here
+      results[numberSqrd - 1] = 1;
+      multiple++;
+    }
+  }
+}
+
+int getAnswer(int numThreads, int maxNumber)
+{
+  results[maxNumber] = {0};
+
+  pthread_t threads[numThreads];
+  pthread_mutex_init(&results_mutex, NULL);
+
+  auto begin_time = chrono::high_resolution_clock::now();
+
+  results[0] = 1;
+  results[1] = 1;
+
+  int init = int(floor(sqrt(maxNumber)));
+  markNonPrimes(2, init);
+
+  for (int i = 0; i < numThreads; i++)
+  {
+    cfg = (Config *)malloc(sizeof(struct Config));
+    cfg->startI = i;
+    cfg->numTrapPerThread = getNumTrapPerThread(i, numThreads, maxNumber);
+    pthread_create(&threads[i], NULL, calculateFactorial, (void *)cfg);
+  }
+
+  for (int j = 0; j < numThreads; j++)
+  {
+    pthread_join(threads[j], NULL);
+  }
+
+  double total = (((b - a) / maxNumber) / 2) * (function(a) + results + function(b));
+  auto end_time = std::chrono::high_resolution_clock::now();
+
+  cout << "result: " << fixed << setprecision(25) << total << "\n";
+  cout << "time taken: " << chrono::duration<double, std::milli>(end_time - begin_time).count() << " ms\n";
+  return 0;
+}
+
+static void show_usage(string name)
+{
+  cerr << "Usage: " << name << "\n"
+       << "Options:\n"
+       << "\t-h,--help\t\t\tShow this help message\n"
+       << "\t-n,--threads\tREQUIRED\tSpecify number of threads\n"
+       << "\t-m,--max-number\tREQUIRED\tSpecify max number"
+       << endl;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 4)
+  {
+    show_usage(argv[0]);
+    return 1;
+  }
+
+  for (int i = 1; i < argc; ++i)
+  {
+    string arg = argv[i];
+    if ((arg == "-h") || (arg == "--help"))
+    {
+      show_usage(argv[0]);
+      return 0;
+    }
+    else if ((arg == "-n") || (arg == "--threads"))
+    {
+      if (i + 1 < argc)
+      {
+        numThreads = stoll(argv[++i]);
+      }
+      else
+      {
+        cerr << "--threads option requires one argument." << endl;
+        return 1;
+      }
+    }
+    else if ((arg == "-m") || (arg == "--max-number"))
+    {
+      if (i + 1 < argc)
+      {
+        maxNumber = stoi(argv[++i]);
+      }
+      else
+      {
+        cerr << "--max-number option requires one argument." << endl;
+        return 1;
+      }
+    }
+  }
+  return getAnswer(numThreads, maxNumber);
+}
