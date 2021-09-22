ebb9a1e975dc175dc05aa9af5a5582678da4fe43
ThameezBo <thameez.bodhanya@allangray.co.za>
2021-09-22 16:53:17 +0200

    fix: add locks

--- Exercises/Assignment2/Q4/fine-grained/sorted_list.hpp
+++ Exercises/Assignment2/Q4/fine-grained/sorted_list.hpp
@@ -1,29 +1,30 @@
 #ifndef lacpp_sorted_list_hpp
 #define lacpp_sorted_list_hpp lacpp_sorted_list_hpp
 
-#include<pthread.h>
-#include<mutex>
+#include <thread>
+#include <mutex>
 
 /* a sorted list implementation by David Klaftenegger, 2015
  * please report bugs or suggest improvements to david.klaftenegger@it.uu.se
  */
 
 /* struct for list nodes */
-template<typename T>
-struct node {
+template <typename T>
+struct node
+{
 	T value;
-	node<T>* next;
+	node<T> *next;
 	std::mutex l;
 };
 
 /* non-concurrent sorted singly-linked list */
-template<typename T>
-class sorted_list {
-	node<T>* first = nullptr;
-	std::mutex l;
+template <typename T>
+class sorted_list
+{
+	node<T> *first = nullptr;
 
-	public:
-		/* default implementations:
+public:
+	/* default implementations:
 		 * default constructor
 		 * copy constructor (note: shallow copy)
 		 * move constructor
@@ -33,83 +34,105 @@ class sorted_list {
 		 * The first is required due to the others,
 		 * which are explicitly listed due to the rule of five.
 		 */
-		sorted_list() = default;
-		sorted_list(const sorted_list<T>& other) = default;
-		sorted_list(sorted_list<T>&& other) = default;
-		sorted_list<T>& operator=(const sorted_list<T>& other) = default;
-		sorted_list<T>& operator=(sorted_list<T>&& other) = default;
-		~sorted_list() {
-			while(first != nullptr) {
-				remove(first->value);
-			}
+	sorted_list() = default;
+	sorted_list(const sorted_list<T> &other) = default;
+	sorted_list(sorted_list<T> &&other) = default;
+	sorted_list<T> &operator=(const sorted_list<T> &other) = default;
+	sorted_list<T> &operator=(sorted_list<T> &&other) = default;
+	~sorted_list()
+	{
+		while (first != nullptr)
+		{
+			remove(first->value);
 		}
-		/* insert v into the list */
-		void insert(T v) {
-			/* first find position */
-			node<T>* pred = nullptr;
-			l.lock();
-			node<T>* succ = first;
-			if (succ) succ->l.lock();
-			l.unlock();
-			
-			/* Check that first is not nullptr*/
-			while(succ != nullptr && succ->value < v) {
-				if(pred) pred->l.unlock();
-				pred = succ;
-				succ = succ->next;
-				if(succ) succ->l.lock();
+	}
+	/* insert v into the list */
+	void insert(T v)
+	{
+		/* first find position */
+		node<T> *pred = nullptr;
+		node<T> *succ = first;
+		if (succ != nullptr)
+		{
+			succ->l.lock();
+		}
+		/* Check that first is not nullptr*/
+		while (succ != nullptr && succ->value < v)
+		{
+			pred = succ;
+			succ->l.unlock();
+			succ = succ->next;
+			if (succ != nullptr)
+			{
+				succ->l.lock();
 			}
-			
-			/* construct new node */
-			node<T>* current = new node<T>();
-			current->value = v;
-			/* insert new node between pred and succ */
-			current->next = succ;
-			if(pred == nullptr) {
-				first = current;
-			} else {
-				pred->next = current;
-			}
-			if(pred) pred->l.unlock();
-			if(current) current->l.unlock();
 		}
 
-		void remove(T v) {
-			/* first find position */
-			node<T>* pred = nullptr;
-			node<T>* current = first;
-			while(current != nullptr && current->value < v) {
-				pred = current;
-				current = current->next;
-			}
-			if(current == nullptr || current->value != v) {
-				/* v not found */
-				return;
-			}
-			/* remove current */
-			if(pred == nullptr) {
-				first = current->next;
-			} else {
-				pred->next = current->next;
-			}
-			delete current;
-		}
+		/* construct new node */
+		node<T> *current = new node<T>();
+		current->value = v;
 
-		/* count elements with value v in the list */
-		std::size_t count(T v) {
-			std::size_t cnt = 0;
-			/* first go to value v */
-			node<T>* current = first;
-			while(current != nullptr && current->value < v) {
-				current = current->next;
-			}
-			/* count elements */
-			while(current != nullptr && current->value == v) {
-				cnt++;
-				current = current->next;
-			}
-			return cnt;
+		/* insert new node between pred and succ */
+		current->next = succ;
+		if (pred == nullptr)
+		{
+			first = current;
 		}
+		else
+		{
+			pred->next = current;
+		}
+		if (succ != nullptr)
+		{
+			succ->l.unlock();
+		}
+	}
+
+	void remove(T v)
+	{
+		/* first find position */
+		node<T> *pred = nullptr;
+		node<T> *current = first;
+		while (current != nullptr && current->value < v)
+		{
+			pred = current;
+			current = current->next;
+		}
+		if (current == nullptr || current->value != v)
+		{
+			/* v not found */
+			return;
+		}
+		/* remove current */
+		if (pred == nullptr)
+		{
+			first = current->next;
+		}
+		else
+		{
+			pred->next = current->next;
+		}
+		delete current;
+	}
+
+	/* count elements with value v in the list */
+	std::size_t count(T v)
+	{
+		std::size_t cnt = 0;
+		/* first go to value v */
+		node<T> *current = first;
+		while (current != nullptr && current->value < v)
+		{
+			current = current->next;
+		}
+		/* count elements */
+		while (current != nullptr && current->value == v)
+		{
+			cnt++;
+			current = current->next;
+		}
+		return cnt;
+	}
 };
 
 #endif // lacpp_sorted_list_hpp
