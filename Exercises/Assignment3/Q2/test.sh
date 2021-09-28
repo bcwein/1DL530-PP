cea83fb34d165ac1bd00e13031b57531d7c77488
Bjørn Christian <bcwein343@gmail.com>
2021-09-28 22:40:41 +0200

    Experiment done.

--- Exercises/Assignment3/Q2/test.sh
+++ Exercises/Assignment3/Q2/test.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 set +e
@@ -6,9 +7,9 @@ rm results.txt
 set -eo pipefail
 
 for t in {1,2,4,8,16}; do
-    for w in {64, 1024, 4096}; do
-        for s in {1000, 2000}; do
-            echo "($t,$w, $s)" >> results.txt
+    for w in {64,1024,4096}; do
+        for s in {1000,2000}; do
+            echo "($t,$w,$s)" >> results.txt
             ./Game_Of_Life $w $s $t >> results.txt
         done
     done
